f4cc13044d518a17d6070a62650c2c69e8e73784
cgreenwood <cagwood@gmail.com>
2019-04-15 14:37:16 -0400

    first commit

---
+++ src/arm-controller.ino
@@ -0,0 +1,63 @@
+// For Arduino Uno
+// Provide 6 registers to be controlled by the Raspberry
+// Pi via I2C for controlling the arm servos.
+// Servo signal pins:
+// JO: pin 3
+// J1: pin 5
+// J2: pin 6
+// J3: pin 9
+// J4: pin 10
+// J5: pin 11
+// See Arduino Uno pinout diagram for SDA and SCL (I2C) pins.
+
+#include <Wire.h>
+#include <Servo.h>
+
+// Index corresponds to joint label number.
+Servo servos[6];
+int servoHomeAngles[] = {90, 150, 20, 0, 0, 0};
+int servoHomeAnglesSize = sizeof(servoHomeAngles)/sizeof(int);
+
+void setup() {
+  // I2C setup
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+
+  // Serial (debug) setup
+  Serial.begin(9600);
+
+  // Servo setup
+  servos[0].attach(3);
+  servos[1].attach(5);
+  servos[2].attach(6);
+  servos[3].attach(9);
+  servos[4].attach(10);
+  servos[5].attach(11);
+
+  // Set servos to home angles
+  for (int i = 0; i < servoHomeAnglesSize; i++) {
+    writeServo(i, servoHomeAngles[i]);
+  }
+}
+
+void loop() {
+  delay(100);
+}
+
+// Protocol: first byte sent is which servo to set.
+// Second byte is the new angle value.
+void receiveEvent(int numBytes) {
+  int servo;
+  int newAngle;
+  while (Wire.available() >= 2) {
+    servo = Wire.read();
+    newAngle = Wire.read();
+    writeServo(servo, newAngle);
+  }
+}
+
+void writeServo(int servo, int newAngle) {
+  servos[servo].write(newAngle);
+  String debugString = "New Angle for J" + String(servo) + ": " + String(newAngle) + "\n";
+  Serial.print(debugString);
+}
---
+++ src/armgui.py
@@ -0,0 +1,88 @@
+#GUI for control of the 3D printed arm
+#Pi must be connected via I2C to the arduino uno, address 8, to
+#control the hardware PWMs. 
+
+from tkinter import *
+
+#Servo Specs and Operation
+#Servos for Joints 0, 1, 2, and 3 are mapped to GPIO pins 29, 31, 33, 35 respectively
+
+def setDutyPWM0(newDuty):
+    print("SetDutyPWM0 called")
+
+def toHome():
+    print("ToHome called")
+
+#UI
+master = Tk()
+master.title("Arm Controller")
+def createSlider(master, label, mini, maxi, init, onChange):
+    slider = Scale(master,
+                   label      = label,
+                   from_      = mini,
+                   to         = maxi,
+                   command    = onChange,
+                   digits     = 3,
+                   resolution = 0.01,
+                   length     = 500,
+                   orient     = HORIZONTAL)
+    slider.set(init)
+    return slider
+
+sliders = []
+for num in range(6):
+    sliders.append(Scale)
+
+sliders[0] = createSlider(master, "J0 Angle", 0, 180, 90, setDutyPWM0)
+sliders[0].pack()
+sliders[1] = createSlider(master, "J1 Angle", 0, 180, 90, setDutyPWM0)
+sliders[1].pack()
+sliders[2] = createSlider(master, "J2 Angle", 0, 180, 90, setDutyPWM0)
+sliders[2].pack()
+sliders[3] = createSlider(master, "J3 Angle", 0, 180, 90, setDutyPWM0)
+sliders[3].pack()
+sliders[4] = createSlider(master, "J4 Angle", 0, 180, 90, setDutyPWM0)
+sliders[4].pack()
+sliders[5] = createSlider(master, "J5 Angle", 0, 180, 90, setDutyPWM0)
+sliders[5].pack()
+
+##j1Scale = Scale(master,
+##    label      = 'J1 servo pulse width (ms)',
+##    from_      = ServoSpecs.pwmDutyMin,
+##    to         = ServoSpecs.pwmDutyMax,
+##    command    = setDutyPWM1,
+##    digits     = 3,
+##    resolution = 0.01,
+##    length     = 500,
+##    orient     = HORIZONTAL)
+##j1Scale.set(ArmSpecs.homeDuties[1])
+##j1Scale.pack()
+##
+##j2Scale = Scale(master,
+##    label      = 'J2 servo pulse width (ms)',
+##    from_      = ServoSpecs.pwmDutyMin,
+##    to         = ServoSpecs.pwmDutyMax,
+##    command    = setDutyPWM2,
+##    digits     = 3,
+##    resolution = 0.01,
+##    length     = 500,
+##    orient     = HORIZONTAL)
+##j2Scale.set(ArmSpecs.homeDuties[2])
+##j2Scale.pack()
+##
+##j3Scale = Scale(master,
+##    label      = 'J3 (wrist) servo pulse width (ms)',
+##    from_      = ServoSpecs.pwmDutyMin,
+##    to         = ServoSpecs.pwmDutyMax,
+##    command    = setDutyPWM3,
+##    digits     = 3,
+##    resolution = 0.01,
+##    length     = 500,
+##    orient     = HORIZONTAL)
+##j3Scale.set(ArmSpecs.homeDuties[3])
+##j3Scale.pack()
+
+Button(master, text='To Home Position', command=toHome).pack()
+
+mainloop()
+#insert cleanup code here
